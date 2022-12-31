fe2c490c1d1852503785bc3ea048138e6dddb7f2
sindhunallam <117097504+sindhunallam@users.noreply.github.com>
2022-12-31 11:34:58 +0530

    Update index.js

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,30 +1,60 @@
+import {formatDistanceToNow} from 'date-fns'
 import './index.css'
 
 const CommentItem = props => {
-  const {eachItem} = props
-  const {Name, Comment, id, isLiked} = eachItem
+  const {eachItem, likefun, deleteComment} = props
+  const {Name, Comment, id, isLiked, time, bgColor} = eachItem
+
+  console.log(bgColor)
+  const deleteeFunall = () => {
+    deleteComment(id)
+  }
+
+  const likecall = () => {
+    likefun(id)
+  }
 
   return (
-    <div className="Item">
+    <li className="Item">
       <div className="heading">
-        <p className="roundedCon">{Name[0]}</p>
+        <p className={`roundedCon ${bgColor}`}>{Name[0].toUpperCase()}</p>
         <p>{Name}</p>
+        <p>{formatDistanceToNow(time)}</p>
       </div>
       <p>{Comment}</p>
       <div className="footer">
         <div className="flexxx">
-          <img src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png" />
-          <p>Like</p>
+          <button type="button" onClick={likecall}>
+            {isLiked ? (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png"
+                alt="liked"
+              />
+            ) : (
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+                alt="like"
+              />
+            )}
+          </button>
+          <p>{isLiked ? 'Liked' : 'Like'}</p>
         </div>
         <div>
-          {' '}
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+          <button
+            type="button"
+            onClick={deleteeFunall}
+            testid="delete"
             alt="delete"
-          />
+          >
+            {' '}
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+              alt="delete"
+            />
+          </button>
         </div>
       </div>
-    </div>
+    </li>
   )
 }
 
